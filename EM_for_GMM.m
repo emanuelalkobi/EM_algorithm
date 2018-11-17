e6a00f470a1ee2f34e75bc921a24c873adf12d9f
XiangZheng <31711517+XiangZheng2017@users.noreply.github.com>
2018-11-16 21:17:18 -0500

    Add files via upload

--- EM_for_GMM.m
+++ EM_for_GMM.m
@@ -1,7 +1,7 @@
 clc
 clear all
 tic 
-rng(1)
+rng(3)
 % precompute
 [x_data,y_data,animals_name] = process_data_zoo();
 [n,d]=size(x_data);
@@ -10,15 +10,15 @@ rng(1)
 [K,~]=size(unique(y_data));
 pi=ones(1,K)/K;
 % mean for each class
-%mu=rand(K,d);
-% choose random 2 in dataset
-mu=x_data(randi(n,K,1),:);
+mu=rand(K,d);
+% choose random K in dataset
+%mu=x_data(randi(n,K,1),:);
 % covariance for each class
 for k =1:K
     sigma(:,:,k)=eye(d,d);
 end
 
-for iter=1:90
+for iter=1:50
     %% E-step
     for k=1:K
         p(:,k)=pi(k)*mvnpdf(x_data,mu(k,:),sigma(:,:,k)); 
@@ -42,18 +42,17 @@ for iter=1:90
     end
    
     [~,predicted]=max(r,[],2);
-    obj_fun(iter)=sum(log(mvnpdf(x_data,mu(predicted,:),sigma(:,:,predicted)))); 
+    ccr(iter)=label_clustring_ccr(predicted,K,y_data);
 end
 
 figure(1)
 [~,predict]=max(r,[],2);
 [s,h] = silhouette(x_data,predict);
-title('silhouette for clustering')
-figure(2)
-plot(obj_fun);
-xlabel('iteration')
-ylabel('value of log likelihood')
-title('log likelihood function')
-
-
-ccr=label_clustring_ccr(predicted,K,y_data)
+%title('silhouette for initialization with K random mean vector and identity convariance')
+title('silhouette for initialization with K random data as mean vector and identity convariance')
+% figure(2)
+% plot(ccr)
+% xlabel('iteration');
+% ylabel('CCR');
+% title('CCR value for each iteration')
+%ccr=label_clustring_ccr(predict,K,y_data);
